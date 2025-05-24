d51532a2515cc987992ae299cb9e0ed990179137
santimontironi <santiimontironi@gmail.com>
2025-05-24 19:23:29 -0300

    se agrego la forma de hacer la peticion pero con async y await

--- promise.js
+++ promise.js
@@ -17,3 +17,21 @@ filePath('./data/archivo1.txt')
     .catch((error) => console.log(error))
 
 //Se usa .then() y .catch() porque la función filePath devuelve una Promesa, ya que está trabajando de forma asincrónica (leer archivos no es inmediato).
+
+//otra forma de hacer esto es con async await:
+
+async function read(){
+    try{
+        const result1 = await filePath('./data/archivo1.txt')
+        const result2 = await filePath('./data/archivo2.txt')
+        const result3 = await filePath('./data/archivo3.txt')
+        console.log(result1)
+        console.log(result2)
+        console.log(result3)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+read()
