f14e2a820c70c3fad7aa4fa3875ee7287d7a6f36
santimontironi <santiimontironi@gmail.com>
2025-05-27 00:49:34 -0300

    aplicando promisify

--- promise.js
+++ promise.js
@@ -1,4 +1,5 @@
 const {readFile} = require('fs')
+const {promisify} = require('util')
 
 function filePath(file){
     return new Promise(function(resolve,reject){
@@ -18,13 +19,17 @@ filePath('./data/archivo1.txt')
 
 //Se usa .then() y .catch() porque la función filePath devuelve una Promesa, ya que está trabajando de forma asincrónica (leer archivos no es inmediato).
 
-//otra forma de hacer esto es con async await:
+//haciendo la promesa con el modulo promisify
+
+const filePathPromise = promisify(readFile)
+
+//usando async await
 
 async function read(){
     try{
-        const result1 = await filePath('./data/archivo1.txt')
-        const result2 = await filePath('./data/archivo2.txt')
-        const result3 = await filePath('./data/archivo3.txt')
+        const result1 = await filePathPromise('./data/archivo1.txt', 'utf-8')
+        const result2 = await filePathPromise('./data/archivo2.txt', 'utf-8')
+        const result3 = await filePathPromise('./data/archivo3.txt', 'utf-8')
         console.log(result1)
         console.log(result2)
         console.log(result3)
